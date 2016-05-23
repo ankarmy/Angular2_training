57be1317906a6d15eb21429f0101dfa8b22e2a63
Alexey Karmanov <akarmanov@qualium-systems.com.ua>
2016-05-23 17:51:05 +0300

    add styles and input field

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>Angular 2</h1>'
+    selector: 'todo-app',
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string;
+    constructor(){
+        this.title = 'Angular 2Do!!!'
+    }
+}
