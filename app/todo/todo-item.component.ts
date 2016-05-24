70b5c0c53168468df18b540c660d6804b7056f22
Alexey Karmanov <akarmanov@qualium-systems.com.ua>
2016-05-24 17:30:02 +0300

    create delete task function

--- app/todo/todo-item.component.ts
+++ app/todo/todo-item.component.ts
@@ -4,11 +4,13 @@ import {Todo} from './todo';
 @Component({
     selector: 'todo-item',
     templateUrl: './app/todo/todo-item.component.html',
-    styleUrls: ['./app/todo/todo-item.component.css']
+    styleUrls: ['./app/todo/todo-item.component.css'],
+    inputs: ['todos']
 })
 
 export class TodoItem {
     @Input() todo: Todo;
+    todos: Todo[];
 
     toggleDone(){
         this.todo.done = !this.todo.done
@@ -16,5 +18,8 @@ export class TodoItem {
 
     delete() {
         console.log('Deleting', this.todo);
+        console.log(this.todos.indexOf(this.todo));
+        let index = this.todos.indexOf(this.todo);
+        this.todos.splice(index, 1);
     }
 }
